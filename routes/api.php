d73d4efa5bcedf24f7eec4392445833ce6c197a5
Rafael Moura <rafaelmoura.dev@gmail.com>
2018-02-17 11:01:03 -0200

    rev

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::middleware('auth:api')->get('/users', function (Request $request) {
-	return implode('|', $request->url());
+	return (string)$request->url();
 });
