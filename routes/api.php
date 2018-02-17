38fdc954c52788b2ce7aee62c78ab2f98a2aaee0
Rafael Moura <rafaelmoura.dev@gmail.com>
2018-02-17 17:42:47 -0200

    rev

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,6 @@ Route::get('users', function (Request $request) {
 });
 
 Route::post('users', function (Request $request) {
-	file_put_contents("php://stderr", "POST USERS: ");// . (string)$request->all());
+	file_put_contents("php://stderr", "POST USERS: " . (string)$request->all());
 	return "";
 });
