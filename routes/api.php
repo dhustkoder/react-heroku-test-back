4c5aaa7e6a3f712100522f3a256d5b3f417a49f9
Rafael Moura <rafaelmoura.dev@gmail.com>
2018-02-18 11:56:08 -0300

    login added

--- routes/api.php
+++ routes/api.php
@@ -63,8 +63,8 @@ Route::post('users/login', function (Request $request) {
 		if ($user == null)
 			return json_encode(response(['error' => 'Login não registrado' ]));
 
-		if ((string)$user['password'] != $request->input('password'))
-			return json_encode(response(['error'] => 'password incorreto'))
+		if ((string)$user['password'] != (string)$request->input('password'))
+			return json_encode(response(['error' => 'password incorreto']))
 
 		return json_encode(response(['success' => 'autenticado']));
 	} catch (Exception $error) {
